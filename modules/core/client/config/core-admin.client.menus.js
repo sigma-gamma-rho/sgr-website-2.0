eee11c527da0fe4a2336df46494e6d8108a6e9af
jeffrey mclemore <jmclemore368@gmail.com>
2016-04-21 16:14:11 -0400

    fix(social, admin): fixed social links, added admin resolve

--- modules/core/client/config/core-admin.client.menus.js
+++ modules/core/client/config/core-admin.client.menus.js
@@ -1,12 +1,16 @@
 'use strict';
 
-angular.module('core.admin').run(['Menus',
-  function (Menus) {
+angular.module('core.admin').run(['Menus', '$rootScope','$window', '$location', '$state',
+  function (Menus, $rootScope, $window, $location, $state) {
     Menus.addMenuItem('topbar', {
       title: 'Admin',
       state: 'admin',
       type: 'dropdown',
       roles: ['admin']
     });
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+      // catch error
+      $state.go('forbidden');
+   });
   }
 ]);
